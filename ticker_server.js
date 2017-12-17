d778996ea6934fcbe531296f345bffb4aa2993a1
Rafael Fragoso <rafael.fragoso@corp.globo.com>
2017-12-16 23:43:40 -0200

    Adding CORS headers

--- ticker_server.js
+++ ticker_server.js
@@ -47,6 +47,18 @@ let exchanges = inputExchanges
 
 
 http.createServer((req, res) => {
+    // CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    if ( req.method === 'OPTIONS' ) {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
     let data = _.chain(exchanges)
         .map((exchange) => {
             return {
